e9eaaa63f69dd5037f63dc1dec0ba5b5e51d6365
Niklas Kühtmann <admin@i-zed.net>
2018-05-18 01:29:49 +0200

    Changed project structure and added rxdart

--- lib/australiasim.dart
+++ lib/australiasim.dart
@@ -1,13 +1,16 @@
 library australiasim;
 
 import "dart:html";
-import 'dart:async';
-import 'package:vector_math/vector_math.dart';
+import "dart:async";
+import "package:vector_math/vector_math.dart";
+import "package:rxdart/rxdart.dart";
 
-part "./actor.dart";
-part "./gamecontroller.dart";
-part "./gamemode.dart";
-part "./gameview.dart";
-part "./world.dart";
+part "./australiasim/actor.dart";
+part "./australiasim/gamecontroller.dart";
+part "./australiasim/gamemode.dart";
+part "./australiasim/gameview.dart";
+part "./australiasim/world.dart";
 
 const cellsize = 50;
+
+final t = new GameController();
