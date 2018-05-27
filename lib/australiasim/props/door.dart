016c6bed799d02c16472a7f574a59422d6defa70
Niklas Kühtmann <admin@i-zed.net>
2018-05-27 23:47:10 +0200

    Added collision handler for door

--- lib/australiasim/props/door.dart
+++ lib/australiasim/props/door.dart
@@ -4,11 +4,21 @@ part of australiasim;
 
 class Door extends Prop
 {
+  
+  StreamController<Pawn> collideEvent = new StreamController();
+  Stream<Pawn> get onCollide => collideEvent.stream.asBroadcastStream();
 
   Door() : super()
   {
       this.name = "Door" + genUID();
       this._rotation = new Vector2(0.0, 1.0); // South
       this._scale = new Vector2(1.0, 0.2);
+      onCollide.listen(handleCollision);
+  }
+
+  handleCollision(Pawn pawn) {
+    if (pawn is Character) {
+      print("You wanna leave already?");
+    }
   }
 }
