6c39c611addab6d414841f482fa36b74de489bdb
Niklas Kühtmann <admin@i-zed.net>
2018-06-01 00:13:55 +0200

    Refactoring.

--- lib/australiasim/pawns/enemy.dart
+++ lib/australiasim/pawns/enemy.dart
@@ -4,21 +4,20 @@ class Enemy extends Pawn
 {
   Enemy() : super()
   {
-      this.name = "Enemy" + genUID();
+    this.name = "Enemy" + genUID();
   }
 
-
-  bool _isEmbattled() => world.gamemode.currentPlayerCharacter != null && world.gamemode.currentPlayerCharacter.location.distanceTo(this.location) < 200.0;
+  bool _isEmbattled() => world.gamemode.player != null && world.gamemode.player.location.distanceTo(this.location) < 200.0;
 
   void tick(double deltaTime)
   {
     if(_isEmbattled())
     {
-        final playerPos = world.gamemode.currentPlayerCharacter.location;
-        final escapeVector = (worldSize / 2.0 - this.location).normalized();
-        this.rotation = this.location + escapeVector * 100.0 - playerPos;
+      final playerPos = world.gamemode.player.location;
+      final escapeVector = (worldSize / 2.0 - this.location).normalized();
+      this.rotation = this.location + escapeVector * 100.0 - playerPos;
 
-        requestWalkToLocation(this.location + this.rotation * 200.0);
+      requestWalkToLocation(this.location + this.rotation * 200.0);
     }
 
     super.tick(deltaTime);
