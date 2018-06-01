ea57a25718e5c59f358e63c36e91e8452a9738a3
Niklas Kühtmann <admin@i-zed.net>
2018-06-01 03:47:38 +0200

    Project restructure

--- lib/australiasim.dart
+++ lib/australiasim.dart
@@ -7,24 +7,27 @@ import "dart:convert";
 import "package:vector_math/vector_math.dart";
 import "package:rxdart/rxdart.dart";
 
-part "./australiasim/gamemode.dart";
-part "./australiasim/actor.dart";
-part "./australiasim/world.dart";
-part "./australiasim/pawn.dart";
-part "./australiasim/prop.dart";
-
-part "./australiasim/domview.dart";
-part "./australiasim/gameview.dart";
-
+// Controller
 part "./australiasim/gamecontroller.dart";
 part "./australiasim/level.dart";
 
-part "./australiasim/props/door.dart";
-part "./australiasim/props/box.dart";
+// View
+part "./australiasim/view/domview.dart";
+part "./australiasim/view/gameview.dart";
 
-part "./australiasim/pawns/character.dart";
-part "./australiasim/pawns/enemy.dart";
+// Model
+part "./australiasim/model/gamemode.dart";
+part "./australiasim/model/actor.dart";
+part "./australiasim/model/world.dart";
+part "./australiasim/model/pawn.dart";
+part "./australiasim/model/prop.dart";
 
-part "./australiasim/pawns/enemies/spider.dart";
+part "./australiasim/model/props/door.dart";
+part "./australiasim/model/props/box.dart";
+
+part "./australiasim/model/pawns/character.dart";
+part "./australiasim/model/pawns/enemy.dart";
+
+part "./australiasim/model/pawns/enemies/spider.dart";
 
 final t = new GameController();
