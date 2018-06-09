b1f6cf5b68b8217ca4db277c9d4e21c4c3518356
Th120 <Th120@spielwieseserver.de>
2018-06-09 21:37:14 +0200

    Cozyness Char lives

--- lib/australiasim/model/pawns/character.dart
+++ lib/australiasim/model/pawns/character.dart
@@ -2,12 +2,31 @@ part of australiasim;
 
 class Character extends Pawn
 {
+ 
+  int _charLives = 2;
+
+  int get charLives => _charLives;
+  set charLives(int lives) => _charLives = lives;
+
   Vector2 _velocity = new Vector2.zero();
 
   Character() : super()
   {
       maxSpeed = 400.0;
       name = "Character";
+
+      new Observable(this.onCollide)
+      .where((Actor a) => a is Enemy)
+      .throttle(new Duration(seconds: 2))
+      .listen( (Actor a) => this._touchedEnemy());
+  }
+
+  void _touchedEnemy()
+  {
+      _charLives = max(_charLives - 1, 0);
+
+      if(charLives == 0)
+        print("DEAD");
   }
 
   get speed => maxSpeed * min(_velocity.length, 100.0) / 100;
--- lib/australiasim/model/pawns/enemy.dart
+++ lib/australiasim/model/pawns/enemy.dart
@@ -7,13 +7,19 @@ class Enemy extends Pawn
   static const double changeDirMinTime = 1.0;
   static const double changeDirMaxTime = 2.5;
 
-  double _recoverTimeRemaining = 0.0;
+  static const double cozynessIncSpeed = 12.0;
+  static const double cozynessDecSpeed = 6.0;
 
+  double _recoverTimeRemaining = 0.0;
   double _changeDirTimeRemaining = 0.0;
+
+  double _currCozyness = 0.0;
 
   final _ran = new Random();
     
   EnemyState get state => _isEmbattled() ? EnemyState.escaping : _recoverTimeRemaining != 0.0 ? EnemyState.postEscape : EnemyState.idle;
+
+  double get cozynessPercent => _currCozyness;
 
   double get speed  
   {
@@ -22,31 +28,36 @@ class Enemy extends Pawn
         case EnemyState.escaping:
             return maxSpeed;
         case EnemyState.postEscape:
-            return maxSpeed * 0.7;
+            return maxSpeed * 0.6;
         case EnemyState.idle:
-            return maxSpeed * 0.38;
+            return maxSpeed * 0.33;
     }     
   }
 
   Enemy() : super()
   {
     this.name = "Enemy" + genUID();
-    onCollide.listen(_collided);
+
+    new Observable(this.onCollide)
+      .throttle(new Duration(milliseconds: 500))
+      .listen( (Actor a) => this._collided(a));
   }
   
   bool _isEmbattled() => world.gamemode.player != null && world.gamemode.player.location.distanceTo(this.location) < 200.0;
+
+  double _nextRandomTime() => _ran.nextDouble() * (changeDirMaxTime - changeDirMinTime).abs() + changeDirMinTime;
 
   void tick(double deltaTime)
   {
     if(_isEmbattled())
     {
-      final playerPos = world.gamemode.player.location;
-      final escapeVector = (worldSize / 2.0 - this.location).normalized();
-      this.rotation = this.location + escapeVector * 70.0 - playerPos;
+        final playerPos = world.gamemode.player.location;
+        final escapeVector = (worldSize / 2.0 - this.location).normalized();
+        this.rotation = this.location + escapeVector * 70.0 - playerPos;
 
-      
+        _recoverTimeRemaining = recoverTime;
 
-      _recoverTimeRemaining = recoverTime;
+        _currCozyness = min(_currCozyness - cozynessDecSpeed * deltaTime, 0.0);
     }
     else
     {
@@ -59,17 +70,22 @@ class Enemy extends Pawn
             if(_changeDirTimeRemaining == 0)
             {
                 _setRandomRotation();
-                _changeDirTimeRemaining = _ran.nextDouble() * (changeDirMaxTime - changeDirMinTime).abs() + changeDirMinTime;
+                _changeDirTimeRemaining = _nextRandomTime();
             }
+
+            _currCozyness = min(_currCozyness + cozynessIncSpeed * deltaTime, 100.0);
         }
         else
         {
-            _changeDirTimeRemaining = _ran.nextDouble() * (changeDirMaxTime - changeDirMinTime).abs() + changeDirMinTime;
+            _changeDirTimeRemaining = _nextRandomTime();
         }
 
     }
 
     requestWalkToLocation(this.location + this.rotation * 200.0);
+
+    if(cozynessPercent == 100.0)
+        print(this.name + ": THIS IS MY HOUSE NOW!");
 
     super.tick(deltaTime);
   }
@@ -84,8 +100,6 @@ class Enemy extends Pawn
       {
           this.rotation = this.location - other.location;
       }
-          
-
   }
 
   void _setRandomRotation()
