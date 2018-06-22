d9225b9e76bde9a774a0a9010b9abe67b615191b
Niklas Kühtmann <admin@i-zed.net>
2018-06-22 04:17:26 +0200

    Updated comments

--- lib/australiasim/view/domview.dart
+++ lib/australiasim/view/domview.dart
@@ -1,76 +1,56 @@
 part of australiasim;
 
-/**
- * DOMView Helper Class for common operations on the DOM.
- */
+/// DOMView is a helper class for common operations on the DOM
 class DOMView {
 
-  /**
-   * Element cache so Elements don't have to be queried multiple times.
-   */
+  /// [Element] cache so Elements don't have to be queried multiple times
   Map<String, Element> _cache = new Map();
 
-  /**
-   * Clears the Cache.
-   */
+  /// Clears the Cache
   clearCache() => _cache.clear();
 
-  /**
-   * Returns a future that resolves after the given Duration.
-   */
-  Future wait(Duration duration) async {
-    return new Future.delayed(duration);
-  }
-
-  /**
-   * Returns a future that resolves before the next animation frame with the given runtime.
-   */
-  Future<num> nextFrame() async {
-    final completer = new Completer<num>();
-    window.requestAnimationFrame((time) => completer.complete(time));
-    return completer.future;
-  }
-
-  /**
-   * Returns a future that resolves after the given Duration 
-   * and calls optional callbacks before and after the Duration.
-   */
-  Future timeout(Duration duration, {Function before, Function after}) async {
-    if ( before != null ) before();
-    await wait(duration);
-    if ( after  != null ) after();
-  }
-
-  /**
-   * Creates a new div with the given id on the host and returns the Element.
-   */
+  /// Creates a new div with the given [id] on the [host] and returns the [Element]
   Element create(Element host, String id) {
     host.appendHtml("<div id='${id}'>");
     return get(id);
   }
 
-  /**
-   * Removes an Element from the DOM by the id.
-   */
+  /// Removes an Element from the DOM by the [id]
   void remove(String id) {
     var el = get(id);
     if (el != null) el.remove();
   }
 
-  /**
-   * Gets an Element from the DOM by the id or from the cache if available.
-   */
+  /// Gets an [Element] from the DOM by the [id] or from the cache if available
   Element get(String id) {
     var el = _cache[id];
     if ( el == null ) el = querySelector("#${id}");
     return el;
   }
 
-  /**
-   * Updates the transform on an Element using optional Vectors.
-   * This function prevents seperate manual transform updates,
-   * that either update rotation or position to override each other.
-   */
+  /// Returns a [Future] that resolves before the next animation frame with the given runtime
+  Future<num> nextFrame() async {
+    final completer = new Completer<num>();
+    window.requestAnimationFrame((time) => completer.complete(time));
+    return completer.future;
+  }
+
+  /// Returns a [Future] that resolves after [Duration] 
+  /// and optionally calls [before] and [after] the Duration.
+  Future timeout(Duration duration, {Function before, Function after}) async {
+    if ( before != null ) before();
+    await wait(duration);
+    if ( after  != null ) after();
+  }
+
+  /// Returns a [Future] that resolves after [duration]
+  Future wait(Duration duration) async {
+    return new Future.delayed(duration);
+  }
+
+  /// Updates the transform on [el] using optional Vectors.
+  /// This function prevents seperate manual transform updates,
+  /// that either update rotatiom, position or scale to override each other.
   void transform(Element el, { Vector2 position, Vector2 rotation, Vector2 scale }) {
     var transformation = "";
 
@@ -94,40 +74,28 @@ class DOMView {
     el.style.transform = transformation;
   }
 
-  /**
-   * Moves the Element to the given position.
-   */
+  // Moves [el] to [position] using css transform
   void move(   Element el, Vector2 position) => transform(el, position:  position);
-  /**
-   * Rotates the Element to the given rotation.
-   */
+
+  // Rotates [el] to [rotation] using css transform
   void rotate( Element el, Vector2 rotation) => transform(el, rotation:  rotation);
-  /**
-   * Scales the Element to the given scale.
-   */
+
+  // Scales [el] to [scale] using css transform
   void scale( Element el, Vector2 scale) => transform(el, scale:  scale);
   
-  /**
-   * Shows the Element.
-   */
+  /// Shows [el] by removing the "hidden" class from it
   void show( Element el ) => el.classes.remove( "hidden" );
-  /**
-   * Hides the Element.
-   */
+
+  /// Hides [el] by adding the "hidden" class to it
   void hide( Element el ) => el.classes.add   ( "hidden" );
 
-  /**
-   * Activates the Element.
-   */
+  /// Activates [el] by adding the "active" class to it
   void activate( Element el ) => el.classes.add( "active" );
-  /**
-   * Deactivates the Element.
-   */
+
+  /// Deactivates [el] by removing the "active" class from it
   void deactivate( Element el ) => el.classes.remove( "active" );
 
-  /**
-   * Sets the width and height of the Element.
-   */
+  // Sets the width and height of [el] to [size]
   void setDimensions(Element el, Vector2 size) {
     el.style.width  = size.x.toString() + "px";
     el.style.height = size.y.toString() + "px";
