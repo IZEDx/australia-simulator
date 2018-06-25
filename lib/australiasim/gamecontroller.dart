2af95fc4926b739361dc0419273ec27faf3b162f
Niklas Kühtmann <admin@i-zed.net>
2018-06-25 23:58:40 +0200

    Clean up and documentation

--- lib/australiasim/gamecontroller.dart
+++ lib/australiasim/gamecontroller.dart
@@ -1,104 +1,132 @@
 part of australiasim;
 
-class GameController {
-  GameMode gameMode;
-  GameView gameView;
-  LevelManager levelManager;
+/// Instantiates model and view, controls the start/end of
+/// the game and passes input from the user onto the model.
+class GameController
+{
 
-  double _lastTick = 0.0;
-  bool _running = false;
+    /// Tick interval
+    static final interval = new Duration(milliseconds: 32);
 
-  GameController() {
+    /// Model instance
+    GameMode gameMode;
 
-    levelManager = new LevelManager("./assets/data/levels.json");
-    gameMode = new GameMode();
-    gameView = new GameView(gameMode, levelManager);
+    /// View instance
+    GameView gameView;
 
-    _init();
-    _setupInput();
-  }
+    /// LevelManager instance
+    LevelManager levelManager;
 
-  _init() async {
-    await levelManager.load();
-    gameView.closeGameView(false);
+    /// Time of the last tick
+    double _lastTick = 0.0;
 
-    /*gameView.get("useGyrosensor").onClick.listen((e) {
-      e.preventDefault();
-      gameView.useGyrosensor = !gameView.useGyrosensor;
-    });*/
+    /// State of the whole game
+    bool _running = false;
 
-    gameView.onSelectLevel.listen((level) => _startGame(level));
+    /// Constructor
+    GameController()
+    {
 
-    gameView.get("selectLevel").onClick.listen((e) {
-        gameView.hide(gameView.get("menu"));
-        gameView.show(gameView.get("levelSelection"));
-    });
+        levelManager = new LevelManager("./assets/data/levels.json");
+        gameMode = new GameMode();
+        gameView = new GameView(gameMode, levelManager);
 
-    gameView.get("showMenu").onClick.listen((e) {
-        gameView.show(gameView.get("menu"));
-        gameView.hide(gameView.get("levelSelection"));
-    });
+        _init(); // Further initializations are asynchronously.
+        
+    }
 
-    gameMode.onGameOver.listen(_gameOver);
+    /// Asynchronous initializer allows waiting for dependencies.
+    _init() async
+    {
+        // Load the levels
+        await levelManager.load();
+        
+        // Show the main menu
+        gameView.closeGameView(false);
 
-  }
+        // Open level selection.
+        gameView.get("selectLevel").onClick.listen((e)
+        {
+            gameView.hide(gameView.get("menu"));
+            gameView.show(gameView.get("levelSelection"));
+        });
 
-  _setupInput() async {
-    gameView.setupInput((touch) {
-      if (_running) gameMode.moveCharacter(touch);
-    });
-    /*await for (var touches in gameView.onInput) {
-      if (running) {
-        await for (var touch in touches) {
-          gameMode.moveCharacter(touch);
+        // Close level selection
+        gameView.get("showMenu").onClick.listen((e)
+        {
+            gameView.show(gameView.get("menu"));
+            gameView.hide(gameView.get("levelSelection"));
+        });
+
+        // Add listener for game start and end
+        gameView.onSelectLevel.listen(_runGame);
+        gameMode.onGameOver.listen(_gameOver);
+
+        // Add listener for movement
+        gameView.setupInput((touch)
+        {
+            if (_running) gameMode.moveCharacter(touch);
+        });
+    }
+
+    /// Starts the game by loading a level and ticking the gamemode.
+    _runGame(Level lvldata) async
+    {
+        // Prevent multiple starts, less necessary than multiple gameovers, but reasonable.
+        if (_running) return;
+        _running = true;
+
+        // Load the level into the gamemode
+        gameMode.load(lvldata);
+        // Open the game view
+        gameView.openGameView();
+        // Start the gamemode
+        gameMode.start();
+        
+        // Show the spawntext of the level for 4 seconds
+        gameView.hintBig(lvldata.spawnText, new Duration(seconds: 4));
+
+        _lastTick = window.performance.now() / 1000; // Start time
+
+        // Keep ticking while the gamemode is running.
+        // The gamemode will run 3 seconds longer after the gameover, keep the enemies moving.
+        while (gameMode.running) {
+            await gameView.timeout(interval); // Wait for interval
+
+            final now = window.performance.now() / 1000;
+            gameMode.tick(now - _lastTick); // Run tick through model with deltatime
+            _lastTick = now; // Reset last tick
         }
-        ;
-      }
-    }*/
-  }
-
-  _startGame(Level lvldata) async {
-    if (!_running) {
-      gameMode.load(lvldata);
-      gameView.openGameView();
-      gameMode.start();
-      _running = true;
-      
-      gameView.hintBig(lvldata.spawnText, new Duration(seconds: 4));
-
-      _lastTick = window.performance.now() / 1000;
-      final interval = new Duration(milliseconds: 32);
-      while (gameMode.running) {
-        await gameView.timeout(interval);
-        final time = window.performance.now() / 1000;
-        gameMode.tick(time - _lastTick);
-        _lastTick = time;
-      }
-    }
-  }
-
-  _gameOver(won) async {
-    if (!_running) return;
-    final charEl = gameView.get("Character");
-    _running = false;
-
-    if (won) {
-      levelManager.current = ++levelManager.current % levelManager.size;
     }
 
-    gameMode.player.walk(new Vector2.zero());
+    /// Ends the game by showing endscreen and animating the character.
+    /// Then the main menu will be shown.
+    _gameOver(won) async
+    {
+        // Prevent multiple gameovers if enemies do stuff after it has ended
+        if (!_running) return;
+        _running = false;
 
-    // Character animations
-    gameView.deactivate(charEl);
-    gameView.timeout(
-      new Duration(milliseconds: 768), // Animation time - 32ms to account for the animation jumping back to start.
-      before: () => charEl.classes.add(won ? "finish-anim" : "dead-anim"), 
-      after: () => charEl.classes.add(won ? "finish" : "dead")
-    );
+        // Update the current level
+        if (won) levelManager.current = ++levelManager.current % levelManager.size;
 
-    await gameView.hintBig(won ? "Well Done!" : "Game Over", new Duration(seconds: 3));
+        // Reset player velocity
+        gameMode.player.walk(new Vector2.zero());
 
-    gameMode.stop();
-    gameView.closeGameView(!won);
-  }
+        // Character animations
+        final charEl = gameView.get("Character");
+        gameView.deactivate(charEl);
+        gameView.timeout(
+        new Duration(milliseconds: 768), // (Animation time - 32ms) to account for the animation jumping back to start.
+            before: () => charEl.classes.add(won ? "finish-anim" : "dead-anim"), 
+            after: () => charEl.classes.add(won ? "finish" : "dead")
+        );
+
+        // Indicate whether the game was won and wait for 3 seconds
+        await gameView.hintBig(won ? "Well Done!" : "Game Over", new Duration(seconds: 3));
+
+        // Stop the gamemode and show the menu
+        gameMode.stop();
+        gameView.closeGameView(!won);
+    }
 }
