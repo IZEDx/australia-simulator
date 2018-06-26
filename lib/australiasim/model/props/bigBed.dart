635e3e2fbb5cb79cd715fe6f7fd0f20ed1b2b460
Niklas Kühtmann <admin@i-zed.net>
2018-06-26 21:12:05 +0200

    Changed name

--- lib/australiasim/model/props/bigBed.dart
+++ lib/australiasim/model/props/bigBed.dart
@@ -6,7 +6,7 @@ class BigBed extends Box
     initialize(World world)
     {
         super.initialize(world);
-        this.name = "Tree" + world.genUID();
+        this.name = "BigBed" + world.genUID();
         this.scale = new Vector2(192.0, 240.0);
         this.rotation = new Vector2(0.0, 1.0);
     }
