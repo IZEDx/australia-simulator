2574beca8b9c6c755159df5ba7f0f313ba7ec8dd
Niklas Kühtmann <admin@i-zed.net>
2018-06-26 18:27:33 +0200

    Refactored level

--- lib/australiasim/level.dart
+++ lib/australiasim/level.dart
@@ -2,38 +2,56 @@ part of australiasim;
 
 
 class LevelManager {
-  bool _ready = false;
-  String _path;
-  List<Level> _levels;
+    bool _ready = false;
+    String _path;
+    List<Level> _levels;
 
-  bool get ready => _ready;
+    bool get ready => _ready;
 
-  String get path => _path;
+    String get path => _path;
 
-  int get size => _levels.length;
+    int get size => _levels.length;
 
-  int get current {
-    final idx = window.localStorage.containsKey("level") ? int.parse(window.localStorage["level"]) : 0;
-    return idx >= size ? size -1 : idx;
-  }
-  set current(int idx) {
-    idx = (idx >= size ? size -1 : idx);
-    window.localStorage["level"] = idx.toString();
-    if (idx > unlocked) _unlocked = idx;
-  }
+    int get current {
+        final idx = window.localStorage.containsKey("level") ? int.parse(window.localStorage["level"]) : 0;
+        return idx >= size ? size -1 : idx;
+    }
+    set current(int idx) {
+        idx = (idx >= size ? size -1 : idx);
+        window.localStorage["level"] = idx.toString();
+        if (idx > unlocked) _unlocked = idx;
+    }
 
-  int get unlocked => window.localStorage.containsKey("unlocked") ? int.parse(window.localStorage["unlocked"]) : 0;
-  set _unlocked(int idx) => window.localStorage["unlocked"] = idx.toString();
+    int get unlocked => window.localStorage.containsKey("unlocked") ? int.parse(window.localStorage["unlocked"]) : 0;
+    set _unlocked(int idx) => window.localStorage["unlocked"] = idx.toString();
 
-  LevelManager(String this._path) {
-  }
+    LevelManager(String this._path) {
+    }
 
-  load() async {
-    _levels = await Level.loadLevels(_path);
-    _ready = true;
-  }
+    load() async {
 
-  Level get(int level) => _levels[level];
+        String body = await HttpRequest.getString(path);
+        List data = JSON.decode(body);
+        if (!(data is List)) return [];
+
+        _levels = [];
+
+        for (final lvldata in data) {
+            if (lvldata is Map && lvldata["path"] != null) {
+                final level = new Level(lvldata["path"]);
+                try {
+                    await level.load();
+                    if (level.loaded) {
+                        _levels.add(level);
+                    }
+                } catch(err) {}
+            }
+        }
+
+        _ready = true;
+    }
+
+    Level get(int level) => _levels[level];
 }
 
 class Level {
@@ -83,30 +101,7 @@ class Level {
 
     this.loaded = true;
   }
-
-  static Future<List<Level>> loadLevels(String path) async {
-    String body = await HttpRequest.getString(path);
-    List data = JSON.decode(body);
-    if (!(data is List)) return [];
-
-    List<Level> levels = [];
-
-    for (final lvldata in data) {
-      if (lvldata is Map && lvldata["path"] != null) {
-        final level = new Level(lvldata["path"]);
-        try {
-          await level.load();
-          if (level.loaded) {
-            levels.add(level);
-          }
-        } catch(err) {}
-      }
-    }
-
-    return levels;
-  }
 }
-
 
 class ActorData {
   Function instance;
