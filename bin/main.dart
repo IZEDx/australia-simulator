727dea1d86585ab1b4ee990e6a123daf8ef19ff8
Niklas Kühtmann <admin@i-zed.net>
2018-06-27 21:44:38 +0200

    Clean up

--- bin/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:start/start.dart';
-
-main() {
-  print("Hosting Web Server on port 3000");
-  start(host: '0.0.0.0', port: 3000).then((Server app) {
-    app.static('./build/web');
-  });
-}
